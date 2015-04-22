1004efb088eaa892f6736a1699ce09093512b8ea
alexmhoule <alexmhoule@gmail.com>
2015-04-22 09:32:17 -0500

    fixing code

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+		$('#add').click(function () {
+			if( $('input#adding').val().trim().length == 0 ) {
+			alert('Whoops something is missing, please type in an item you need to purchase');
+		} else {
+			$('#myList').append('<li><button class="remove">Remove</button><input type="checkbox">' + $('input#adding').val() + '</li>');
+			$('input#adding').val("");
+		}
+		});
+});
