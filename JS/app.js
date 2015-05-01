b2e40f1429d43895c44cbc89da9d565d7bea86c6
alexmhoule <alexmhoule@gmail.com>
2015-05-01 01:24:54 -0500

    changes made

--- JS/app.js
+++ JS/app.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
 			alert('Whoops something is missing, please type in an item you need to purchase');
 		} else 
 		 {
-			$('#myList').append('<li><button class="remove">Remove</button><input type="checkbox">' + $('input#adding').val() + '</li>');
+			$('#myList').append('<li><button class="remove">Remove</button><input type="checkbox" class="test">' + $('input#adding').val() + '</li>');
 			$('input#adding').val("");
 		}
 		});
 		$('ul').on("click", ".remove", function () {
 			$(this).parent().remove();
 		});
-		
+		$('ul').on("click", ".test", function () {
+			$(this).closest('li').toggleClass("strikethrough");
+		});
 });
